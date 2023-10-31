7b73d82a0ecc087bd72d482942c4a727b6339a5c
Santeri <santeri.kaaranto@gmail.com>
2023-10-31 18:43:48 +0200

    Kommenttien lisäys

--- src/services/items.js
+++ src/services/items.js
@@ -1,24 +1,28 @@
-import axios from 'axios'
+import axios from "axios";
 
-const BASE_URL = 'http://localhost:3001/items'
+const BASE_URL = "http://localhost:3001/items";
 
-const getAll  = () => {
-    const request = axios.get(BASE_URL)
-    return request.then(response => response.data)
+// Noutaa kaikki tiedot json palvelimelta
+const getAll = () => {
+  const request = axios.get(BASE_URL);
+  return request.then((response) => response.data);
 };
 
+// Poistaa palvelimelta tiedon annetun ID:n mukaan
 const deleteData = (id) => {
-    const request = axios.delete(`${BASE_URL}/${id}`)
-    return request.then(response => response.data)
+  const request = axios.delete(`${BASE_URL}/${id}`);
+  return request.then((response) => response.data);
 };
 
-const addData  = (newNoteObject) => {
-    const request = axios.post(BASE_URL, newNoteObject)
-    return request.then(response => response.data)
+// Lisää dataa palvelimelle
+const addData = (newNoteObject) => {
+  const request = axios.post(BASE_URL, newNoteObject);
+  return request.then((response) => response.data);
 };
 
+// Export
 export default {
-  getAll : getAll,
+  getAll: getAll,
   deleteData: deleteData,
-  addData: addData
+  addData: addData,
 };
